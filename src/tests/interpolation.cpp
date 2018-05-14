cf772ee1b8a5baf07b28886d150713ca50b29b89
R. Harish Navnit <harishnavnit@gmail.com>
2018-05-14 19:40:49 +0530

    Port the test to use QVariantList container.

--- src/tests/interpolation.cpp
+++ src/tests/interpolation.cpp
@@ -1,19 +1,19 @@
-#include <QDebug>
 #include <QVector>
-#include <QApplication>
+#include <QVariant>
 #include <QGeoCoordinate>
+#include <QCoreApplication>
 
 #include "lineinterpolator.h"
 
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
-    QVector<QGeoCoordinate> points;
-    points.append(QGeoCoordinate(59.9483, 10.7695));
-    points.append(QGeoCoordinate(59.9645, 10.6712));
-    points.append(QGeoCoordinate(60.0321, 10.8553));
+    QVariantList points;
+    points.append(QVariant::fromValue(QGeoCoordinate(59.9483, 10.7695)));
+    points.append(QVariant::fromValue(QGeoCoordinate(59.9645, 10.6712)));
+    points.append(QVariant::fromValue(QGeoCoordinate(60.0321, 10.8553)));
 
     LineInterpolator li(points);
     li.populateSegments();
